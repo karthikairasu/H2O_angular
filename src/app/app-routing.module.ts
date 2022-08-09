25dd9da3dde6379d67a3555ddbc26035200fc707
abs-karthikairasuk-ui <65330823+abs-karthikairasuk-ui@users.noreply.github.com>
2022-08-09 12:58:42 +0530

    routes pages checking

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
+import { LayoutComponent } from './layout/layout.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
@@ -16,3 +17,7 @@ const routes: Routes = [
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
+// { path: '', redirectTo: 'home', pathMatch: 'full' },
+// { path: 'home', component: HomeComponent },
+// { path: 'dashboard', component: DashboardComponent },
+// { path: 'login', component: LoginComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import { MatButtonModule } from '@angular/material/button';
     SidenavComponent,
     HomeComponent,
     LoginComponent,
+    LayoutComponent,
   ],
   imports: [
     BrowserModule,
